e9ec72d5d4bb1097277d2229b5eb2b1a7f670e3a
francobb <jrizzle1790@gmail.com>
2022-06-28 09:50:00 -0400

    another one

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -15,7 +15,7 @@ class UsersRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.usersController.getUsers);
+    this.router.get(`${this.path}`, authMiddleware, this.usersController.getUsers);
     this.router.get(`${this.path}/:id`, authMiddleware, this.usersController.getUserById);
     this.router.post(`${this.path}`, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
     this.router.put(`${this.path}/:id`, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
