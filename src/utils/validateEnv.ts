0cc3cca2df0c570878166e2eaee60851013ffcc7
francobb <jrizzle1790@gmail.com>
2023-03-08 16:04:36 -0500

    add log

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,6 +1,7 @@
 import { cleanEnv, port, str } from 'envalid';
 
 const validateEnv = () => {
+  console.log('validating env' + { prc: process.env });
   cleanEnv(process.env, {
     NODE_ENV: str(),
     PORT: port(),
