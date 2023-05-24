ed157ee0bc9690984e542d64beb10b831dfce1a6
buteaufrancoisjr <jrizzle1790@gmail.com>
2023-05-23 22:28:04 -0400

    test dirname

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,6 +4,7 @@ import winston from 'winston';
 import winstonDaily from 'winston-daily-rotate-file';
 import { LOG_DIR } from '@config';
 
+console.log(__dirname, LOG_DIR);
 // logs dir
 const logDir: string = join(__dirname, LOG_DIR);
 
