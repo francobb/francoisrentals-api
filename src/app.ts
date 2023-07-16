3876593ba332044f17d091cd399d01fd9066c8a5
buteaufrancoisjr <jrizzle1790@gmail.com>
2023-07-16 19:24:09 -0400

    fix(requests): images are uploaded to s3
    Use Multer S3 dependency to upload images passed from the client with maintenance requests.

--- src/app.ts
+++ src/app.ts
@@ -94,16 +94,12 @@ class App {
     const swaggerDefinition = {
       openapi: '3.0.0',
       info: {
-        title: 'Express API for JSONPlaceholder',
+        title: 'API for Francois Rentals',
         version: '1.0.0',
-        description: 'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+        description: 'This is the Swagger UI for Francois Rentals.',
         license: {
           name: 'Licensed Under MIT',
           url: 'https://spdx.org/licenses/MIT.html',
-        },
-        contact: {
-          name: 'JSONPlaceholder',
-          url: 'https://jsonplaceholder.typicode.com',
         },
       },
       servers: [
