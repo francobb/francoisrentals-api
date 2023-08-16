52e0f27cb15abf861edd5249723b74afbb0f6871
buteaufrancoisjr <jrizzle1790@gmail.com>
2023-08-16 17:01:27 -0400

    refactor: some cleanup

--- jest.integration.config.ts
+++ jest.integration.config.ts
@@ -4,8 +4,6 @@ const { compilerOptions } = require('./tsconfig.json');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // globalSetup: '<rootDir>/intTests/setup/globalSetup.ts',
-  // globalTeardown: '<rootDir>/intTests/setup/globalTeardown.ts',
   setupFilesAfterEnv: ['<rootDir>/intTests/setup/setupFile.ts'],
   roots: ['<rootDir>/intTests'],
   testPathIgnorePatterns: ['<rootDir>/test'],
