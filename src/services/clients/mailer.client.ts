b99a1b67f8cb5c4ab0644547712e8677eaddd0a7
buteaufrancoisjr <jrizzle1790@gmail.com>
2023-09-26 22:23:18 -0400

    chore: update stuff

--- src/services/clients/mailer.client.ts
+++ src/services/clients/mailer.client.ts
@@ -1,6 +1,7 @@
 import nodemailer from 'nodemailer';
 import { EMAIL_ADDRESS, EMAIL_PASSWORD } from '@config';
 
+console.log({ EMAIL_ADDRESS, EMAIL_PASSWORD });
 const mailer = nodemailer.createTransport({
   service: 'gmail',
   auth: {
