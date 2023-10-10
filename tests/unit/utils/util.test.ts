ff6ae22dacb5bfd6c37f3453c9d0ff7d9f47aec4
buteaufrancoisjr <jrizzle1790@gmail.com>
2023-10-10 15:34:00 -0400

    test: fix broken test

--- tests/unit/utils/util.test.ts
+++ tests/unit/utils/util.test.ts
@@ -3,7 +3,7 @@ import { getLastDayOfMonth } from '@utils/util';
 describe('util', function () {
   describe('getLastDayOfMonth', function () {
     it('should expect it to return last day of a month', function () {
-      expect(getLastDayOfMonth('2023', '03')).toEqual(31);
+      expect(getLastDayOfMonth(2023, 3)).toEqual(31);
     });
   });
 });
