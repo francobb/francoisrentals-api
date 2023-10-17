828ae72d204f22e0ed5bc5838744e37201cecc0f
buteaufrancoisjr <jrizzle1790@gmail.com>
2023-10-16 20:06:46 -0400

    chore(pm2): update configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@
 module.exports = {
   apps: [
     {
-      name: 'prod', // pm2 start App name
+      name: 'fr-prod', // pm2 start App name
       script: 'dist/server.js',
       exec_mode: 'cluster', // 'cluster' or 'fork'
       instance_var: 'INSTANCE_ID', // instance variable
@@ -26,7 +26,7 @@ module.exports = {
       },
     },
     {
-      name: 'dev', // pm2 start App name
+      name: 'fr-dev', // pm2 start App name
       script: 'ts-node', // ts-node
       args: '-r tsconfig-paths/register --transpile-only src/server.ts', // ts-node args
       exec_mode: 'cluster', // 'cluster' or 'fork'
