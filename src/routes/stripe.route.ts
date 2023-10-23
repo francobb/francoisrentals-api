87677c8c249583d83c2e972ac1638b9142b5373c
buteaufrancoisjr <jrizzle1790@gmail.com>
2023-10-23 16:45:33 -0400

    refactor(middleware): maintenance & rent payment
    requests must come from ui-client

--- src/routes/stripe.route.ts
+++ src/routes/stripe.route.ts
@@ -1,7 +1,7 @@
 import StripeController from '@/controllers/stripe.controller';
 import { Routes } from '@interfaces/routes.interface';
 import { Router } from 'express';
-import authMiddleware, { checkRole } from '@middlewares/auth.middleware';
+import authMiddleware, { checkClient, checkRole } from '@middlewares/auth.middleware';
 
 class StripeRoute implements Routes {
   public path = '/payment';
@@ -14,8 +14,14 @@ class StripeRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}/transactions`, authMiddleware, checkRole(['ADMIN', 'TENANT']), this.stripeController.getTransactions);
-    this.router.post(`${this.path}`, authMiddleware, checkRole(['ADMIN', 'TENANT']), this.stripeController.receiveRentPayment);
-    this.router.post(`${this.path}/request`, authMiddleware, checkRole(['ADMIN', 'TENANT']), this.stripeController.receivePaymentRequest);
+    this.router.post(`${this.path}`, checkClient, authMiddleware, checkRole(['ADMIN', 'TENANT']), this.stripeController.receiveRentPayment);
+    this.router.post(
+      `${this.path}/request`,
+      checkClient,
+      authMiddleware,
+      checkRole(['ADMIN', 'TENANT']),
+      this.stripeController.receivePaymentRequest,
+    );
     this.router.post(`${this.path}/webhook`, this.stripeController.processStripeWebhook);
   }
 }
