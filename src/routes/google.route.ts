abbf573be9dad2d660a917d27fe5d4e9d38b5885
buteaufrancoisjr <jrizzle1790@gmail.com>
2023-11-26 16:21:12 -0500

    chore: minor cleanup

--- src/routes/google.route.ts
+++ src/routes/google.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { Routes } from '@interfaces/routes.interface';
 import GoogleController from '@controllers/google.controller';
-import { authWithGoogle, authWithGoogleCallback, checkRole, requireJwtAuth } from '@middlewares/auth.middleware';
+import { Routes } from '@interfaces/routes.interface';
+import { authWithGoogle, authWithGoogleCallback, checkClient, checkRole, requireJwtAuth } from '@middlewares/auth.middleware';
 
 class GoogleRoute implements Routes {
   public path = '/';
@@ -13,11 +13,16 @@ class GoogleRoute implements Routes {
     this.initializeRoutes();
   }
   private initializeRoutes() {
+    this.router.get(`${this.path}getAuthUrl`, this.googleController.getAuthUrl);
+
+    /* FR-RN-Client */
+    this.router.post(`${this.path}auth/google/client`, checkClient, this.googleController.handleClientAuthRequest);
+    this.router.get(`${this.path}listFiles`, requireJwtAuth, checkRole(['ADMIN']), this.googleController.getFilesFromDrive);
+
     /* passport google routes */
     this.router.get(`${this.path}google`, authWithGoogle);
     this.router.get(`${this.path}auth/google/callback`, authWithGoogleCallback, this.googleController.googleOauth20Handler);
     /* passport google routes */
-    this.router.get(`${this.path}listFiles`, requireJwtAuth, checkRole(['ADMIN']), this.googleController.getFilesFromDrive);
   }
 }
 
