d70534e0f1c66b5587af8baeca13e1b1932a2114
francobb <jrizzle1790@gmail.com>
2024-08-04 16:51:36 -0400

    chore: add firebase authentication; update search query dates

---
+++ src/services/clients/firebase.client.ts
@@ -0,0 +1,8 @@
+import { ServiceAccount} from 'firebase-admin/app';
+import * as admin from "firebase-admin";
+
+import serviceAccount from "./serviceaccount.json"
+
+export const firebaseApp = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+});
--- src/services/transactions.service.ts
+++ src/services/transactions.service.ts
@@ -1,29 +1,53 @@
 import transactionsModel from '@models/transactions.model';
 import { ITransaction } from '@interfaces/transactions.interface';
 import { logger } from '@utils/logger';
-import reportModel from '@models/report.model';
-import { IReport } from '@interfaces/report.interface';
 import SearchQueryBuilder from '@search/transactions.search';
-import { IFile, IQuery } from '@utils/interfaces';
+import {  IQuery } from '@utils/interfaces';
+
+type IDate = string | number | Date;
 
 class TransactionService {
   public transactions = transactionsModel;
 
   public async searchTransaction(query: IQuery) {
-    const month = Number(query.month ?? undefined);
-    const year = Number(query.year ?? undefined);
-    const location = query.location ?? undefined;
-    const outcome = query.outcome ?? undefined;
-    const payeePayer = query.payeePayer ?? undefined;
-    const sq = new SearchQueryBuilder().withDate({ month, year }).withLocation(location).withOutcome(outcome).withPayeePayer(payeePayer).build();
+    const { from, to } = query;
+    let { location, outcome, payeePayer } = query;
+
+    location = location ?? undefined;
+    outcome = outcome ?? 'income';
+    payeePayer = payeePayer ?? undefined;
+
+    const queryDates = getDateRange(from, to);
+    const sq = new SearchQueryBuilder().withDate(queryDates).withLocation(location).withOutcome(outcome).withPayeePayer(payeePayer).build();
     const [transactions] = await Promise.all([this.transactions.find(sq).lean()]);
+
     return transactions;
   }
+
   public async addManyTransactions(transactions: ITransaction[]) {
     this.transactions.insertMany(transactions, (error, result) => {
       error ? logger.error(error) : logger.info(':::::transactions inserted:::::', result);
     });
   }
 }
+
+const getDateRange = (from: IDate, to: IDate) => {
+  const today = new Date();
+  const thirtyDaysAgo = new Date(today);
+  thirtyDaysAgo.setDate(today.getDate() - 30);
+
+  let fromDate = from ? new Date(from) : thirtyDaysAgo;
+  let toDate = to ? new Date(to) : today;
+
+  if (from && !to) {
+    toDate = new Date(fromDate);
+    toDate.setDate(fromDate.getDate() + 30);
+  } else if (to && !from) {
+    fromDate = new Date(toDate);
+    fromDate.setDate(toDate.getDate() - 30);
+  }
+
+  return { from: fromDate, to: toDate };
+};
 
 export default TransactionService;
