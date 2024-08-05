f11792a07cd84e53e8320d580293df2ec6a77e74
francobb <jrizzle1790@gmail.com>
2024-08-04 20:14:37 -0400

    test: fiix integration test

--- src/services/clients/firebase.client.ts
+++ src/services/clients/firebase.client.ts
@@ -1,7 +1,6 @@
 import { ServiceAccount } from 'firebase-admin/app';
 import * as admin from 'firebase-admin';
 
-import serviceAccount from './serviceaccount.json';
 import { CLIENT_EMAIL, FR_FIREBASE_PROJECT_ID, FR_FIREBASE_PRIVATE_NEW_KEY, FR_FIREBASE_CLIENT_ID, FR_FIREBASE_PRIVATE_NEW_KEY_ID } from '@config';
 
 export const firebaseApp = admin.initializeApp({
@@ -10,11 +9,11 @@ export const firebaseApp = admin.initializeApp({
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     type: 'service_account',
-    project_id: FR_FIREBASE_PROJECT_ID,
-    private_key_id: FR_FIREBASE_PRIVATE_NEW_KEY_ID,
-    private_key: FR_FIREBASE_PRIVATE_NEW_KEY,
-    client_email: CLIENT_EMAIL,
-    client_id: FR_FIREBASE_CLIENT_ID,
+    project_id: FR_FIREBASE_PROJECT_ID || "fakeApp",
+    private_key_id: FR_FIREBASE_PRIVATE_NEW_KEY_ID || "fakeApp",
+    private_key: FR_FIREBASE_PRIVATE_NEW_KEY || "fakeApp",
+    client_email: CLIENT_EMAIL || "fakeApp@email.com",
+    client_id: FR_FIREBASE_CLIENT_ID || "fakeApp",
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
     token_uri: 'https://oauth2.googleapis.com/token',
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
