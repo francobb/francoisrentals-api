98967549a696bf68ae403f90a95b4c6d08afbd02
francobb <jrizzle1790@gmail.com>
2024-08-04 20:17:48 -0400

    test: fiix integration test

--- src/services/clients/firebase.client.ts
+++ src/services/clients/firebase.client.ts
@@ -8,11 +8,11 @@ export const firebaseApp = admin.initializeApp({
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     type: 'service_account',
-    project_id: FR_FIREBASE_PROJECT_ID || "fakeApp",
-    private_key_id: FR_FIREBASE_PRIVATE_NEW_KEY_ID || "fakeApp",
-    private_key: FR_FIREBASE_PRIVATE_NEW_KEY || "fakeApp",
-    client_email: CLIENT_EMAIL || "fakeApp@email.com",
-    client_id: FR_FIREBASE_CLIENT_ID || "fakeApp",
+    project_id: FR_FIREBASE_PROJECT_ID || 'fakeApp',
+    private_key_id: FR_FIREBASE_PRIVATE_NEW_KEY_ID || 'fakeApp',
+    private_key: FR_FIREBASE_PRIVATE_NEW_KEY || 'fakeApp',
+    client_email: CLIENT_EMAIL || 'fakeApp@email.com',
+    client_id: FR_FIREBASE_CLIENT_ID || 'fakeApp',
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
     token_uri: 'https://oauth2.googleapis.com/token',
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
