f8ea88d907f77811534f9236affe0405bac00c02
francobb <jrizzle1790@gmail.com>
2024-08-04 20:00:13 -0400

    test: fiix integration test

--- src/services/clients/firebase.client.ts
+++ src/services/clients/firebase.client.ts
@@ -2,7 +2,22 @@ import { ServiceAccount } from 'firebase-admin/app';
 import * as admin from 'firebase-admin';
 
 import serviceAccount from './serviceaccount.json';
+import {CLIENT_EMAIL, FR_FIREBASE_PROJECT_ID, FR_FIREBASE_PRIVATE_NEW_KEY, FR_FIREBASE_CLIENT_ID, FR_FIREBASE_PRIVATE_NEW_KEY_ID} from "@config";
 
 export const firebaseApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  // credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  credential: admin.credential.cert({
+    // @ts-ignore
+    "type": "service_account",
+    "project_id": FR_FIREBASE_PROJECT_ID,
+    "private_key_id": FR_FIREBASE_PRIVATE_NEW_KEY_ID,
+    "private_key": FR_FIREBASE_PRIVATE_NEW_KEY,
+    "client_email": CLIENT_EMAIL,
+    "client_id": FR_FIREBASE_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-kw94n%40coughee-pot.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  })
 });
